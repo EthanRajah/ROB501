a2d21aaca71640be42ad65eebedbb8229cea79ea
EthanRajah <ethan.rajah@outlook.com>
2024-09-29 01:24:34 -0400

    histogram equalization computation added

--- A1/templates/histogram_eq.py
+++ A1/templates/histogram_eq.py
@@ -21,6 +21,26 @@ def histogram_eq(I):
     # Verify I is grayscale.
     if I.dtype != np.uint8:
         raise ValueError('Incorrect image format!')
+    
+    J = np.zeros_like(I)
+    histogram_dict = {}
+    
+    # Store count of each grey level value in histogram_dict
+    for i in range(I.shape[0]):
+        for j in range(I.shape[1]):
+            if (histogram_dict.get(I[i, j]) == None):
+                histogram_dict[I[i, j]] = 1
+            else:
+                histogram_dict[I[i, j]] += 1
+    
+    # Calculate cumulative distribution function for each intensity level. Each index of culm_dist is a cumulative sum of all previous intensity levels
+    culm_dist = (1/(I.shape[0]*I.shape[1])) * np.cumsum([histogram_dict.get(i, 0) for i in range(256)])
+
+    # Map intensity levels to new values
+    for i in range(I.shape[0]):
+        for j in range(I.shape[1]):
+            # Scale pixel intensity to 0-255 based on cumulative distribution
+            J[i, j] = round(255 * culm_dist[I[i, j]]).astype(np.uint8)
 
     #------------------
 
